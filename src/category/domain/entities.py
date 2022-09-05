30952739e9fe5649987e5260ddd0f4a0ed2d423d
João Otávio <joaotavioos@outlook.com>
2022-09-04 21:02:36 -0300

    feat: Initial commit	🎉

---
+++ src/category/domain/entities.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+@dataclass()  # init, repr, eq
+class Category:
+
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=lambda: datetime.now()
+    )
