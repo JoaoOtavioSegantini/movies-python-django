647c6b1ea165f8c19e75bdd70dbf52827c76e13c
João Otávio <joaotavioos@outlook.com>
2022-09-17 10:23:07 -0300

    test: add dto tests 🚨

--- src/category/tests/integration/domain/test_int_entities.py
+++ src/category/tests/integration/domain/test_int_entities.py
@@ -2,6 +2,7 @@ import unittest
 from category.domain.entities import Category
 from _seedworker.domain.exceptions import EntityValidationException
 
+
 class TestCategoryIntegration(unittest.TestCase):
 
     def test_create_with_invalid_cases_for_name_prop(self):
